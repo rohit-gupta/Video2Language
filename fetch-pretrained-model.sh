bd7f8b411e99794ad16bf8d5c417cf409ce0ee7c
Rohit Gupta <rohitgupta.hpf@gmail.com>
2018-03-25 10:09:57 +0000

    add language model to model fetching script

--- fetch-pretrained-model.sh
+++ fetch-pretrained-model.sh
@@ -1,3 +1,4 @@
 wget -O models/entity_simple_tag_model_regularized.h5 https://s3.ap-south-1.amazonaws.com/rohitgupta-models/entity_simple_tag_model_regularized.h5
 wget -O models/action_simple_tag_model_regularized.h5 https://s3.ap-south-1.amazonaws.com/rohitgupta-models/action_simple_tag_model_regularized.h5
 wget -O models/attribute_simple_tag_model_regularized.h5 https://s3.ap-south-1.amazonaws.com/rohitgupta-models/attribute_simple_tag_model_regularized.h5
+wget -O models/medvoc_simplepredtags_batch128_lowdropout_avgfeat_threshold_maxvalacc.h5 https://s3.ap-south-1.amazonaws.com/rohitgupta-models/medvoc_simplepredtags_batch128_lowdropout_avgfeat_threshold_maxvalacc.h5
